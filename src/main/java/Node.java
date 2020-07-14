1b321aebbfd5edab4af3c81fc044a58b456681d7
charl <charl.wei@shein.com>
2020-07-14 11:02:57 +0800

    单链表

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,8 +1,15 @@
-package PACKAGE_NAME;
-
 /**
+ * 单链表
  * Create by charl
  * Date 2020/7/14 11:00
  */
-public class Node {
+public class Node<T> {
+
+    private T date;
+
+    private Node next;
+
+    public Node(T date) {
+        this.date = date;
+    }
 }
